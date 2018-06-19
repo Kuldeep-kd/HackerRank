b68523a7ea96ba49831551dfacf4f31795fb814e
Kuldeep Thakre <kuldeep-kd@live.com>
2018-06-20 00:06:08 +0530

    ProblemSolutionAccepted

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -10,6 +10,27 @@ namespace HackerRank
     {
         static void Main(string[] args)
         {
+            plusMinus(new int[] { -4, 3, -9, 0, 4, 1 });
+        }
+
+        static void plusMinus(int[] arr)
+        {
+            double d1, d2, d3;
+            d1 = d2 = d3 = 0;
+            foreach (int i in arr)
+            {
+                if (i > 0)
+                    d1++;
+                else if (i < 0)
+                    d2++;
+                else if(i==0)
+                    d3++;
+            }
+            int x = arr.Length;
+            Console.Write("{0:###.#######} \n{1:###.#######} \n{2:###.#######}",(d1/x), d2/x, d3/x);
+            
+            
+
         }
     }
 }
---
+++ StairCase/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StairCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            staircase(3);
+            Console.ReadKey();
+        }
+
+        static void staircase(int n)
+        {
+            int i=1, j=1;
+            string s="";
+            while(i<=n)
+            {
+                j = n - i;
+                while(j>=1)
+                {
+                    s += " ";
+                    j--;
+                }
+                j = 1;
+                while (j<=i)
+                {
+                    s += "*";
+                    j++;
+                }
+                Console.WriteLine(s);
+                s = "";
+                i++;
+            }
+            
+        }
+    }
+}
